42a67fdc9ea7c5f5973239ce5efc1ccbdcce5987
GalinaUm <GalinaUmerzakhova@yandex.ru>
2026-05-01 21:25:34 +0300

    feat: Add Review and Ad urls

--- ads/urls.py
+++ ads/urls.py
@@ -1,2 +1,17 @@
-from django.urls import path
-urlpatterns = []
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import AdViewSet, ReviewViewSet
+
+router = DefaultRouter()
+router.register(r'ads', AdViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('ads/<int:ad_id>/reviews/',
+         ReviewViewSet.as_view({'get': 'list', 'post': 'create'})
+         ),
+    path('ads/<int:ad_id>/reviews/<int:pk>/',
+         ReviewViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})
+         ),
+]
